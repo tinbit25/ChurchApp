f633b77c4b9e24b23209506dc6d10cf7ea12d4d0
tinbit25 <tinbitelias25@gmail.com>
2025-04-01 15:50:20 +0300

    finish users functionality

--- users/models.py
+++ users/models.py
@@ -1,3 +1,4 @@
+
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
@@ -8,16 +9,3 @@ class User(AbstractUser):
         ('member', 'Member'),
     )
     role = models.CharField(max_length=10, choices=ROLE_CHOICES, default='member')
-
-    # Adding unique related_name to avoid clash with default User model
-    groups = models.ManyToManyField(
-        'auth.Group',
-        related_name='user_set_custom',  # Change the related_name here
-        blank=True
-    )
-    
-    user_permissions = models.ManyToManyField(
-        'auth.Permission',
-        related_name='user_permissions_set_custom',  # Change the related_name here
-        blank=True
-    )
--- users/views.py
+++ users/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from django.contrib.auth import get_user_model
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework.permissions import IsAuthenticated
+from .serializers import UserSerializer
 
-# Create your views here.
+User = get_user_model()
+
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+class ProfileView(APIView):
+    permission_classes = [IsAuthenticated]
+    def get(self, request):
+        serializer = UserSerializer(request.user)
+        return Response(serializer.data)
